99e851996469b5995e9aac5340bfffea3f25258f
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-05-28 20:44:24 +0100

    refactor of profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,18 @@
 from flowmeer_db.permissions import IsOwnerOrReadOnly
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status 
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 
 # Create your views here.
-class ListProfile(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True,
-                                       context={'request': request})
-        return Response(serializer.data)
-    
-    
-class DetailProfile(APIView):
-    serializer_class = ProfileSerializer
+class ListProfile(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer   
+
+class DetailProfile(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self,pk):
-        try:
-            profile=Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-        
-    def get(self, request, pk):
-        profile= self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request':request})
-        return Response(serializer.data)
-    
-    def put(self,request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request':request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer 
