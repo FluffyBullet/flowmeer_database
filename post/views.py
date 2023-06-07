2903123ffd585ae1b57e55a09fe44bd0138c6908
FluffyBullet <davidreynolds34@hotmail.co.uk>
2023-06-07 21:04:45 +0000

    addition of search parameters in post_views

--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
-from rest_framework import permissions
+from django.db.models import Count
+from rest_framework import permissions, filters
 from rest_framework import generics
+from djang_filters.rest_framework impoort DjangoFilterBackend
 from .models import Post
 from .serializers import PostSerializer
 from flowmeer_db.permissions import IsOwnerOrReadOnly
@@ -13,7 +15,24 @@ class ListPost(generics.ListCreateAPIView):
 
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        votes_count=Count('votes', distinct=True),
+        comments_count=Count('comment', dinstict=True)
+    ).order_by('created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__vote__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'votes_count',
+        'comments_count',
+        'votes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner = self.request.user)
