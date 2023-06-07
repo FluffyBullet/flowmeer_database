6d26e3ef2f9875f07c3473b30856349733a93e84
FluffyBullet <davidreynolds34@hotmail.co.uk>
2023-06-07 20:46:28 +0000

    url updated to pass search term on post

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,5 @@ from post import views
 urlpatterns = [
     path('post/', views.ListPost.as_view()),
     path('post/<int:pk>/', views.DetailPost.as_view()),
-    path('post/search/', views.SearchPost.as_view()),
+    path('post/search=<str:query>', views.SearchPost.as_view()),
 ]
