9839cc7deb0b47c81a2daf152df65ac2c505f3bc
FluffyBullet <davidreynolds34@hotmail.co.uk>
2023-06-14 19:12:31 +0000

    additional feeds to post serializer to include votes and comment count

--- post/serializers.py
+++ post/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Post
+from votes.models import Vote
 
 class PostSerializer(serializers.ModelSerializer):
     owner= serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    votes_id = serializers.SerializerMethodField()
+    votes_count = serializers.ReadOnlyField()
+    comment_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -31,5 +35,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'image', 'flower_tag'
+            'title', 'image', 'flower_tag', 'votes_id', 'votes_count',
         ]
