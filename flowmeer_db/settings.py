2e2b85abb31ece16831761d919600eaeed43c1a3
FluffyBullet <davidreynolds34@hotmail.co.uk>
2023-06-14 19:25:53 +0000

    include debug in settings file

--- flowmeer_db/settings.py
+++ flowmeer_db/settings.py
@@ -36,6 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'flowmeerdb.herokuapp.com','127.0.0.1', os.environ.get('ALLOWED_HOST')]
 
